049cc93f3c5d9b79930e5f21a7b38852d11d284b
JT <hello@jtolds.com>
2011-01-06 16:13:47 -0700

    literals work

--- src/parser.cpp
+++ src/parser.cpp
@@ -13,12 +13,14 @@ namespace parser {
   using namespace uscheme::ast;
   
   template <typename Iterator>
-  struct grammar : qi::grammar<Iterator, Integer(), ascii::space_type>{
+  struct grammar : qi::grammar<Iterator, Variable(), ascii::space_type>{
     qi::rule<Iterator, Body(), ascii::space_type> body;
     qi::rule<Iterator, Binding(), ascii::space_type> definition;
     qi::rule<Iterator, PTR<Expression>(), ascii::space_type> expression;
     qi::rule<Iterator, Integer(), ascii::space_type> integer;
     qi::rule<Iterator, Boolean(), ascii::space_type> boolean;
+    qi::rule<Iterator, std::string(), ascii::space_type> string_value;
+    qi::rule<Iterator, std::string(), ascii::space_type> identifier;
     qi::rule<Iterator, String(), ascii::space_type> string;
     qi::rule<Iterator, Variable(), ascii::space_type> variable;
     qi::rule<Iterator, Conditional(), ascii::space_type> conditional;
@@ -29,8 +31,14 @@ namespace parser {
     qi::rule<Iterator, Body(), ascii::space_type> begin;
     qi::rule<Iterator, Application(), ascii::space_type> application;
 
-    grammar() : grammar::base_type(integer) {
+    grammar() : grammar::base_type(variable) {
       integer = qi::long_long[phx::bind(&Integer::value, qi::_val) = qi::_1];
+      string_value = qi::lexeme['"' >> +(qi::char_ - '"') >> '"'];
+      string = string_value[phx::bind(&String::value, qi::_val) = qi::_1];
+      boolean = qi::lit("true")[phx::bind(&Boolean::value, qi::_val) = true] 
+              | qi::lit("false")[phx::bind(&Boolean::value, qi::_val) = false];
+      identifier = qi::alpha >> *qi::alnum;
+      variable = identifier[phx::bind(&Variable::name, qi::_val) = qi::_1];
     }
   };
   
@@ -67,13 +75,13 @@ int main(int argc, char** argv) {
   str = os.str();
   
   uscheme::parser::grammar<std::string::const_iterator> g;
-  uscheme::ast::Integer b;
-  b.value = -1;
+  uscheme::ast::Variable b;
+  b.name = "junk";
   std::string::const_iterator iter = str.begin();
   std::string::const_iterator end = str.end();
   bool r = boost::spirit::qi::phrase_parse(iter, end, g,
       boost::spirit::ascii::space, b);
-  if(r && iter == str.end()) { std::cout << "yay " << b.value << std::endl; }
+  if(r && iter == str.end()) { std::cout << "yay " << b.name << std::endl; }
   // do stuff
   return 0;
 }
